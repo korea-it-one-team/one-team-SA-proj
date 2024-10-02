4bd8c45c513bb0dc7cec7cf41743ca1a3c30e37d
JerryKiki <gasi758@gmail.com>
2024-10-02 16:06:11 +0900

    first commit

---
+++ src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
@@ -0,0 +1,13 @@
+package com.lyj.proj.oneteamsaproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTeamSaProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTeamSaProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyj.proj.oneteamsaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTeamSaProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
