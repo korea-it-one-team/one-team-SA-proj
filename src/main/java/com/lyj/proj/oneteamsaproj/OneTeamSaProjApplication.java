e25191f1a5b9e11204b7489ce7acfe4b1b539ea7
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-02 16:22:03 +0900

    1231231212

--- src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
+++ src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
@@ -8,6 +8,7 @@ public class OneTeamSaProjApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OneTeamSaProjApplication.class, args);
+        System.out.println("1231231123");
     }
 
 }
