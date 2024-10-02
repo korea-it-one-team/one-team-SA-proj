ab9e13b1f923e2df5246f0b120e98f7e908f38f4
JerryKiki <gasi758@gmail.com>
2024-10-02 16:36:47 +0900

    test

--- src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
+++ src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
@@ -8,7 +8,7 @@ public class OneTeamSaProjApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OneTeamSaProjApplication.class, args);
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 
 }
