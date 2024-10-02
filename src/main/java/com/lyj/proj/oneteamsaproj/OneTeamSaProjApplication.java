eeefd557062e3e5d28d2ed70826244578186a1ed
JerryKiki <gasi758@gmail.com>
2024-10-02 16:11:26 +0900

    roll-back-develop

--- src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
+++ src/main/java/com/lyj/proj/oneteamsaproj/OneTeamSaProjApplication.java
@@ -8,7 +8,6 @@ public class OneTeamSaProjApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OneTeamSaProjApplication.class, args);
-        System.out.println("hi");
     }
 
 }
